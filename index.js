6eeb0e0d249f436c9917d3d1c3eca689f0b48d69
Brad Whitesell <whitesellbt@gmail.com>
2021-12-11 17:19:10 -0800

    feat: made uwu bot great again

--- index.js
+++ index.js
@@ -1,9 +1,87 @@
 const Discord = require("discord.js");
 const fs = require("fs");
-const uwufy = require("uwufy");
 
 const config = JSON.parse(fs.readFileSync("./config.json"));
 const client = new Discord.Client();
+
+const kaomojis = [
+  `. ( ^ ω ^)`,
+  `. (っ˘ω˘ς )`,
+  `. ٩(◕‿◕｡)۶`,
+  `. (o･ω･o)`,
+  `. (ღ˘⌣˘ღ)`,
+  `. ヾ(・ω・)`,
+  `. (づ ◕‿◕ )づ`,
+  `. (つ≧▽≦)つ`,
+  `. (・ω・)ﾉ`,
+  `. ( ◡‿◡ *)`,
+  `. (✿◠‿◠)`,
+  `. (✿◡‿◡)`,
+];
+
+const asterisksInjections = [
+  `. \\\**cums*\\\*`,
+  `. \\\**notices your bulge*\\\*`,
+  `. \\\**blushes*\\\*`,
+  `. \\\**motions to waitress, "Czech please!"*\\\*`,
+  `. \\\**nuzzles you*\\\*`,
+  `. \\\**glomp*\\\*`,
+];
+
+// Function for uwuing strings.
+const uwufy = (message) => {
+  // Replace lower case ls and rs with lower case w.
+  message = message.replace(/(?:l|r)/g, "w");
+
+  // Replace upper case Ls and Rs with upper case W.
+  message = message.replace(/(?:L|R)/g, "W");
+
+  // Replace 'th' with 'ff'.
+  message = message.replace(/(?:th )/gi, "ff ");
+
+  // Replace 'fuck' (case insensitive) with 'fucky wucky'.
+  message = message.replace(/(?:fuck)/gi, "fucky wucky");
+
+  // Replace bangs with uwu face.
+  message = message.replace(/!+/g, "! >w< ");
+
+  // Replace the character 'n' followed by any vowel with 'ny'.
+  const nyaExpression = new RegExp(/(?:n[aeiou])/, "g");
+  let nyaExpressionMatch;
+
+  while ((nyaExpressionMatch = nyaExpression.exec(message)) !== null) {
+    message = message.substring(0, nyaExpressionMatch.index) + "ny" + message.substring(nyaExpressionMatch.index + 1);
+  }
+
+  // Replace periods with either asterisks injections or kaomojis.
+  const periodExp = new RegExp(/\.+/, "g");
+  let periodExpMatch;
+
+  while ((periodExpMatch = periodExp.exec(message)) !== null) {
+    let asterisksInjection = Math.random() < 0.5;
+
+    if (asterisksInjection) {
+      message =
+        message.substring(0, periodExpMatch.index) +
+        asterisksInjections[Math.floor(Math.random() * asterisksInjections.length)] +
+        message.substring(periodExpMatch.index + 1);
+      continue;
+    }
+
+    message = message.substring(0, periodExpMatch.index) + kaomojis[Math.floor(Math.random() * kaomojis.length)] + message.substring(periodExpMatch.index + 1);
+  }
+
+  // Randome chance to stutter.
+  let stutter = Math.random() < 0.5;
+
+  // If we should stutter add a copy of the first character of the message and put a dash after it.
+  if (stutter) {
+    let firstChar = message.charAt(0);
+    message = firstChar + "-" + message;
+  }
+
+  return message;
+};
 
 // Check our config data before proceeding.
 if (!config.loginToken) {
@@ -56,6 +134,11 @@ client.on("message", (msg) => {
     return;
   }
 
+  // Random chance to uwu the message.
+  if (!(Math.random() < 0.05)) {
+    return;
+  }
+
   // UwUify the message.
   const uwuReply = uwufy(msg.content);
 
@@ -68,7 +151,7 @@ client.on("message", (msg) => {
   // Reply with uwu'd message.
   msg.channel
     .send(uwuReply, {
-      tts: true,
+      tts: false,
     })
     .then(() => {
       console.log(`${new Date().toUTCString()}: Uwufy message sent successfully. ${msg.id}`);
